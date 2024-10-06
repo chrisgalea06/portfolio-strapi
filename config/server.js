15836937830cb1d4f04683d8acc0bede02815dbd
Christopher Galea <christophergalea@ChristophersMBP.lan>
2024-10-06 21:43:40 +0200

    Updated config

--- config/server.js
+++ config/server.js
@@ -1,10 +1,18 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env("PUBLIC_URL", "https://admin.christophergalea.com"),
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: env.array("APP_KEYS"),
   },
 });
