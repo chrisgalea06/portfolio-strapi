c376b573649e886a4edf3b00dcc237db6c44f4b2
Christopher Galea <christophergalea@ChristophersMBP.lan>
2024-10-07 17:05:43 +0200

    Updated prod config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   proxy: true,
   host: "0.0.0.0",
   port: process.env.PORT,
-  url: env("MY_HEROKU_URL"),
+  url: "https://admin.christophergalea.com", // Public URL for the Strapi instance
   app: {
     keys: env.array("APP_KEYS"),
   },
